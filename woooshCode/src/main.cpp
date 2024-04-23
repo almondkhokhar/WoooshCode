65d8bad3a0b1b228cc88bb99788b8ef25dd27ea9
almondkhokhar <153022020+almondkhokhar@users.noreply.github.com>
2024-04-23 10:17:59 -0400

    All autos no skills

--- woooshCode/src/main.cpp
+++ woooshCode/src/main.cpp
@@ -11,35 +11,26 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
-#include <math.h>
-#include <iostream> 
-#define Pi 3.14159265358979323846
-#define fieldscale 1.66548042705
-#define SL 5 //distance from tracking center to middle of left wheel
-#define SR 5 //distance from tracking center to middle of right wheel
-#define SS 7.75 //distance from tracking center to middle of the tracking wheel
-#define WheelDiam 3.25 //diameter of all the wheels being used for tracking
-#define tpr 360  //Degrees per single encoder rotation
-double DeltaL,DeltaR,DeltaB,currentL,currentR,PreviousL,PreviousR,DeltaTheta,X,Y,Theta,DeltaXSide,DeltaYSide,SideChord,OdomHeading;
+double DeltaL, DeltaR, DeltaB, currentL, currentR, PreviousL, PreviousR, DeltaTheta, X, Y, Theta, DeltaXSide, DeltaYSide, SideChord, OdomHeading;
 // initializes variables
-class Drive{
-  public:
-    double wheelRatio;
-    double gearRatio;
-    double kp;
-    double kd;
-    double initialAngle;
-    double finalAngle;
-    double totalError;
-    double currentError;
-    double finalDistance;
-    double currentDistance;
-    double turningDistance;
-    double turnTimeout;
-    double totalTimeout;
-    double firstDriveTimeout;
-    double secondDriveTimeout;
-
+class Drive
+{
+public:
+  double wheelRatio;
+  double gearRatio;
+  double kp;
+  double kd;
+  double initialAngle;
+  double finalAngle;
+  double totalError;
+  double currentError;
+  double finalDistance;
+  double currentDistance;
+  double turningDistance;
+  double turnTimeout;
+  double totalTimeout;
+  double firstDriveTimeout;
+  double secondDriveTimeout;
 };
 bool conToggle1 = true;
 bool conToggle2 = true;
@@ -52,7 +43,7 @@ bool rbwingtoggle = false;
 bool lbwingtoggle = false;
 bool Shoottogg = false;
 bool punchythingToggle = false;
-bool dropdowntoggle=false;
+bool dropdowntoggle = false;
 bool hangToggle = false;
 bool f1loop = true;
 bool f2loop = true;
@@ -70,10 +61,10 @@ int autoNum = 6;
 
 void pre_auton(void)
 {
-  //Calibrates gyro for auton
+  // Calibrates gyro for auton
   Inertial.calibrate();
   wait(3, sec);
-  //Says when you are ready
+  // Says when you are ready
   con.Screen.print("Ready");
   // Brain.Screen.print("Selected: Slot 1");
   // Brain.Screen.setCursor(2,1);
@@ -99,19 +90,19 @@ void pre_auton(void)
   // Brain.Screen.setCursor(7,1);
   // Brain.Screen.print("FINALS NO BAR TOUCH");
 
-  Brain.Screen.print("Selected: Slot 3");
-  Brain.Screen.setCursor(2,1);
-  Brain.Screen.print("Check Air");
-  Brain.Screen.setCursor(3,1);
-  Brain.Screen.print("Four Ball Near Side");
-  Brain.Screen.setCursor(4,1);
-  Brain.Screen.print("Setting Tool 2");
-  Brain.Screen.setCursor(5,1);
-  Brain.Screen.print("Set Tool Edge on Goofy Corner");
-  Brain.Screen.setCursor(6,1);
-  Brain.Screen.print("Triball On Left Wing");
-  Brain.Screen.setCursor(7,1);
-  Brain.Screen.print("FINALS NO BAR TOUCH");
+  // Brain.Screen.print("Selected: Slot 3");
+  // Brain.Screen.setCursor(2, 1);
+  // Brain.Screen.print("Check Air");
+  // Brain.Screen.setCursor(3, 1);
+  // Brain.Screen.print("Four Ball Near Side");
+  // Brain.Screen.setCursor(4, 1);
+  // Brain.Screen.print("Setting Tool 2");
+  // Brain.Screen.setCursor(5, 1);
+  // Brain.Screen.print("Set Tool Edge on Goofy Corner");
+  // Brain.Screen.setCursor(6, 1);
+  // Brain.Screen.print("Triball On Left Wing");
+  // Brain.Screen.setCursor(7, 1);
+  // Brain.Screen.print("FINALS NO BAR TOUCH");
 
   // Brain.Screen.print("Selected: Slot 4");
   // Brain.Screen.setCursor(2,1);
@@ -131,7 +122,7 @@ void pre_auton(void)
   // Brain.Screen.setCursor(2,1);
   // Brain.Screen.print("Check Air");
   // Brain.Screen.setCursor(3,1);
-  // Brain.Screen.print("Five Ball Near Side");
+  // Brain.Screen.print("Five Ball Far Side");
   // Brain.Screen.setCursor(4,1);
   // Brain.Screen.print("Setting Tool 1");
   // Brain.Screen.setCursor(5,1);
@@ -139,21 +130,21 @@ void pre_auton(void)
   // Brain.Screen.setCursor(6,1);
   // Brain.Screen.print("Triball On Left Wing");
   // Brain.Screen.setCursor(7,1);
-  // Brain.Screen.print("Touches Bar");
+  // Brain.Screen.print("NO TOUCH");
 
-  // Brain.Screen.print("Selected: Slot 6");
-  // Brain.Screen.setCursor(2,1);
-  // Brain.Screen.print("Check Air");
-  // Brain.Screen.setCursor(3,1);
-  // Brain.Screen.print("Five Ball Far Side");
-  // Brain.Screen.setCursor(4,1);
-  // Brain.Screen.print("Setting Tool 2");
-  // Brain.Screen.setCursor(5,1);
-  // Brain.Screen.print("NOT YET CREATED");
-  // Brain.Screen.setCursor(6,1);
-  // Brain.Screen.print("Triball On Right Wing");
-  // Brain.Screen.setCursor(7,1);
-  // Brain.Screen.print("Touches Bar MAYBE");
+  Brain.Screen.print("Selected: Slot 6");
+  Brain.Screen.setCursor(2,1);
+  Brain.Screen.print("Check Air");
+  Brain.Screen.setCursor(3,1);
+  Brain.Screen.print("Five Ball Far Side");
+  Brain.Screen.setCursor(4,1);
+  Brain.Screen.print("Setting Tool 2");
+  Brain.Screen.setCursor(5,1);
+  Brain.Screen.print("NOT YET CREATED");
+  Brain.Screen.setCursor(6,1);
+  Brain.Screen.print("Triball On Right Wing");
+  Brain.Screen.setCursor(7,1);
+  Brain.Screen.print("Touches Bar MAYBE");
 
   // Brain.Screen.print("Selected: Slot 7");
   // Brain.Screen.setCursor(2,1);
@@ -182,59 +173,67 @@ void pre_auton(void)
   // Brain.Screen.print("Triball On Right Wing");
   // Brain.Screen.setCursor(7,1);
   // Brain.Screen.print("NO BAR TOUCH");
-  //Auton selector with potentiometer
+  // Auton selector with potentiometer
 }
 
-int lfQuickfire(){
+int lfQuickfire()
+{
   leftwing.open();
   wait(.2, sec);
   leftwing.close();
   return (0);
 }
-int lfSlow(){
+int lfSlow()
+{
   wait(.2, sec);
   leftwing.open();
-  return(0);
+  return (0);
 }
-int rfQuickfire(){
+int rfQuickfire()
+{
   rightwing.open();
   wait(.2, sec);
   rightwing.close();
   return (0);
 }
-int slowDrop(){
+int slowDrop()
+{
   wait(.1, sec);
   rDropDown.open();
-  return(0);
+  return (0);
 }
-
+int slowerDrop()
+{
+  wait(.25, sec);
+  rDropDown.open();
+  return (0);
+}
 void AWPDefense()
 {
-  //removes triball
+  // removes triball
   rDropDown.open();
-  Drive.move(5 , .3);
-  Drive.turn(-39 , 1.2);
+  Drive.move(5, .3);
+  Drive.turn(-39, 1.2);
   rDropDown.close();
-  //Shoots matchload
-  wait(.6 , sec);
-  intake.spin(fwd,-100,pct);
+  // Shoots matchload
+  wait(.6, sec);
+  intake.spin(fwd, -100, pct);
   wait(7, sec);
   rightwing.open();
-  Drive.move(42 , 2);
-  wait(.5 , sec);
+  Drive.move(42, 2);
+  wait(.5, sec);
   rightwing.close();
-  
 }
-//Clears triballs starting on our side and removes one triball from the corner on the left side of the red goal
-//Setup: Front left wheel in the notch of the field tiles intake up and aimed at the goal red triballs: one with midline at the back of the wing other with one corner in the blue goal
+// Clears triballs starting on our side and removes one triball from the corner on the left side of the red goal
+// Setup: Front left wheel in the notch of the field tiles intake up and aimed at the goal red triballs: one with midline at the back of the wing other with one corner in the blue goal
 
-
-void doNothing(){
-  //does nothing
+void doNothing()
+{
+  // does nothing
 }
 
-
-void fiveMidFar() {
+void fiveMidFarset1()
+{
   int startTime = vex::timer::system();
   task rwing = task(rfQuickfire);
   intake.spin(fwd, 100, pct);
@@ -254,7 +253,7 @@ void fiveMidFar() {
   Drive.move(52, 1.2);
   Drive.turn(55, .6);
   rDropDown.open();
-  intake.spin(fwd,-100, pct);
+  intake.spin(fwd, -100, pct);
   Drive.move(18, .5);
   Drive.turn(10, .5);
   rDropDown.close();
@@ -266,7 +265,7 @@ void fiveMidFar() {
   Drive.move(20, .9);
   Drive.move(-50, 1);
   Drive.turn(60, .7);
-  intake.spin(fwd,-100, pct);
+  intake.spin(fwd, -100, pct);
   leftwing.open();
   Drive.swing(30, .7, .6, 3, false);
   Drive.move(1000, .3);
@@ -276,28 +275,66 @@ void fiveMidFar() {
   Drive.move(1000, .4);
   Drive.move(-10, .4);
 
-  
-
-
-
-
-  
-  printf("%lu\n",(vex::timer::system()-startTime));
+  printf("%lu\n", (vex::timer::system() - startTime));
 }
+void fiveMidFarset2()
+{
+  int startTime = vex::timer::system();
+  task rwing = task(rfQuickfire);
+  intake.spin(fwd, 100, pct);
+  Drive.move(57, 1.05);
+  Drive.move(-8, .5);
+  Drive.turn(120, .8);
+  intake.spin(fwd, -100, pct);
+  wait(.15,sec);
+  Drive.move(1000, .6);
+  Drive.move(-30, .7);
+  intake.spin(fwd, 100, pct);
+  Drive.turn(255, .7);
+  Drive.move(20, .65);
+  Drive.turn(170, .65);
 
-void sixMidFar(){
+  Drive.move(52, 1);
+  Drive.turn(80, .6);
+  rDropDown.open();
+  intake.spin(fwd, -100, pct);
+  Drive.move(18, .5);
+  Drive.turn(35, .4);
+  rDropDown.close();
+  wait(.1, sec);
+  Drive.turn(-85, .78);
+  Drive.move(40, .78);
+  Drive.turn(-52, .5);
+  intake.spin(fwd, 100, pct);
+  Drive.move(22, .9);
+  Drive.move(-52, 1);
+  Drive.turn(85, .7);
+  intake.spin(fwd, -100, pct);
+  leftwing.open();
+  Drive.swing(30, .7, .6, 3, false);
+  Drive.move(1000, .3);
+  Drive.move(-10, .2);
+  leftwing.close();
+  printf("%lu\n", (vex::timer::system() - startTime));
+  wait(.2, sec);
+  Drive.move(1000, .4);
+  Drive.move(-10, .4);
+
+}
+void sixMidFar()
+{
   int startTime = vex::timer::system();
   task rwing = task(rfQuickfire);
   intake.spin(fwd, 100, pct);
   Drive.move(60, 1.2);
 
-
-  printf("%lu\n",(vex::timer::system()-startTime));
+  printf("%lu\n", (vex::timer::system() - startTime));
 }
-void fourMidNearset1(){
+void fourMidNearset1()
+{
   int startTime = vex::timer::system();
   task lwing = task(lfQuickfire);
-  intake.spin(fwd,100,pct);
+  intake.spin(fwd, 100, pct);
   Drive.move(49, 1);
   Drive.move(-54, 1.2);
   Drive.turn(70, 1);
@@ -322,23 +359,24 @@ void fourMidNearset1(){
   rightwing.open();
   Drive.move(36, .8);
   Drive.turn(80, .7);
-  rightwing.close();
   Drive.move(29, .8);
   Drive.turn(77, .4);
-  Drive.move(-40, .8);
+  Drive.move(-42, .9);
   Drive.turn(130, .4);
-  printf("%lu\n",(vex::timer::system()-startTime));
+  printf("%lu\n", (vex::timer::system() - startTime));
 }
-void fourMidNearset2(){
+
+void fourMidNearset2()
+{
   int startTime = vex::timer::system();
   task lwing = task(lfQuickfire);
   intake.spin(fwd, 100, pct);
   Drive.swing(65, 1.4, .86, 3, false);
   Drive.move(-20, .7);
-  Drive.turn(-160, .8);
-  Drive.move(58, 1.2);
+  Drive.turn(-160, .9);
+  Drive.move(58, 1.3);
   Drive.turn(-240, .7);
-  task dropSlow = task(slowDrop);
+  task dropSlow = task(slowerDrop);
   Drive.move(17, .6);
   Drive.turn(-300, .5);
   rDropDown.close();
@@ -346,22 +384,30 @@ void fourMidNearset2(){
   wait(.5, sec);
   Drive.turn(-260, .7);
   Drive.move(-27, .7);
-  Drive.turn(-210, .5);
+  Drive.turn(-210, .7);
   rDropDown.open();
-  Drive.move(-20, .6);
+  Drive.move(-8, .6);
   Drive.turn(-215, .5);
-  intake.spin(fwd, 100, pct);
+  Drive.move(10, .5);
+  intake.spin(fwd, -100, pct);
   rDropDown.close();
-  Drive.move(30, .8);
+  Drive.turn(-240, .5);
+  Drive.move(29, .8);
   Drive.turn(-300, .7);
-
-  printf("%lu\n",(vex::timer::system()-startTime));
+  rightwing.open();
+  Drive.move(34, .9);
+  Drive.move(-46, 1.2);
+  rightwing.close();
+  Drive.turn(-280, .6);
+  
+  printf("%lu\n", (vex::timer::system() - startTime));
 }
 
-void fiveMidNear(){
+void fiveMidNear()
+{
   int startTime = vex::timer::system();
   task lwing = task(lfQuickfire);
-  intake.spin(fwd,100,pct);
+  intake.spin(fwd, 100, pct);
   Drive.move(50, 1);
   Drive.turn(80, .6);
   task slowlwing = task(lfSlow);
@@ -390,28 +436,26 @@ void fiveMidNear(){
   Drive.move(12, .6);
   Drive.turn(75, .6);
   intake.spin(fwd, -100, pct);
-  Drive.move(33, .9);
+  Drive.move(35, .9);
   Drive.turn(77, .3);
-  // Drive.move(-40, .8);
-  // Drive.turn(130, .3);
-  // Drive.move(-10, .5);
- 
-  printf("%lu\n",(vex::timer::system()-startTime));
+  Drive.move(-40, .8);
+  Drive.turn(130, .3);
+  Drive.move(-10, .5);
 
-}
-void testing() {
-  Drive.swing(65, 15, .6, 3, false);
+  printf("%lu\n", (vex::timer::system() - startTime));
 }
 
-void (*autonsList[])()=
+
+void (*autonsList[])() =
 {
-  fourMidNearset2,
-  doNothing, 
-  fiveMidFar,
-  fourMidNearset1,
-  testing,
+  fiveMidFarset2,
+  doNothing,
+  fourMidNearset2, 
   fiveMidNear,
+  fourMidNearset1,
+  fiveMidFarset1,
   sixMidFar,
+
 };
 
 void autonomous()
@@ -419,166 +463,172 @@ void autonomous()
   autonsList[autoSelect]();
 }
 
-int conInfo(){
-  while (true){
-  con.Screen.clearScreen();
-  con.Screen.setCursor(1,1);
-  con.Screen.print("Battery:");
-  con.Screen.print(Brain.Battery.capacity(percent));
-  con.Screen.setCursor(2,1);
-  con.Screen.print("Left  ");
-  con.Screen.print((left1.temperature(fahrenheit)+left2.temperature(fahrenheit)+left3.temperature(fahrenheit))/3);
-  con.Screen.setCursor(3,1);
-  con.Screen.print("Right  ");
-  con.Screen.print((right1.temperature(fahrenheit)+right2.temperature(fahrenheit)+right3.temperature(fahrenheit))/3);
-  wait(1, sec);
+int conInfo()
+{
+  while (true)
+  {
+    con.Screen.clearScreen();
+    con.Screen.setCursor(1, 1);
+    con.Screen.print("Battery:");
+    con.Screen.print(Brain.Battery.capacity(percent));
+    con.Screen.setCursor(2, 1);
+    con.Screen.print("Left  ");
+    con.Screen.print((left1.temperature(fahrenheit) + left2.temperature(fahrenheit) + left3.temperature(fahrenheit)) / 3);
+    con.Screen.setCursor(3, 1);
+    con.Screen.print("Right  ");
+    con.Screen.print((right1.temperature(fahrenheit) + right2.temperature(fahrenheit) + right3.temperature(fahrenheit)) / 3);
+    wait(1, sec);
   }
-  return(0);
+  return (0);
 }
 void usercontrol()
 {
- //r2 intake reverse hold,
- // r1 intake fwd hold,
- // l1 shoot toggle,
- // l2 hang hold tap to fire piston then,
- //y right frontwing,
- // b right backwing,
- // right arrow front leftwing
- // down arrow back leftwing
- task printcon = task(conInfo);
- while (true)
- {
-   // driving portion
-   // controls the speed at which the robot moves
-   //moving controls
-   rightdrive.spin(fwd, con.Axis2.value(), pct);
-   leftdrive.spin(fwd, con.Axis3.value(), pct);
-   //intake controls
-   if (con.ButtonR2.pressing()){
-     intake.spin(fwd, -100, pct);
-   }
-   else if (con.ButtonR1.pressing()){
-     intake.spin(fwd, 100, pct);
-   }
-   else{
-     intake.stop(hold);
-   }
-   //kicker controls
+  // r2 intake reverse hold,
+  //  r1 intake fwd hold,
+  //  l1 shoot toggle,
+  //  l2 hang hold tap to fire piston then,
+  // y right frontwing,
+  //  b right backwing,
+  //  right arrow front leftwing
+  //  down arrow back leftwing
+  task printcon = task(conInfo);
+  while (true)
+  {
+    // driving portion
+    // controls the speed at which the robot moves
+    // moving controls
+    rightdrive.spin(fwd, con.Axis2.value(), pct);
+    leftdrive.spin(fwd, con.Axis3.value(), pct);
+    // intake controls
+    if (con.ButtonR2.pressing())
+    {
+      intake.spin(fwd, -100, pct);
+    }
+    else if (con.ButtonR1.pressing())
+    {
+      intake.spin(fwd, 100, pct);
+    }
+    else
+    {
+      intake.stop(hold);
+    }
+    // kicker controls
 
     if (con.ButtonL2.pressing())
-   {
-    if (f6loop){
-      f6loop=false;
-      if (f7loop){
-        hang.open();
-        con.rumble(".-..--.--..");
-        f7loop=false;
+    {
+      if (f6loop)
+      {
+        f6loop = false;
+        if (f7loop)
+        {
+          hang.open();
+          con.rumble(".-..--.--..");
+          f7loop = false;
+        }
+        else
+        {
+          kicker.spin(fwd, -100, pct);
+          kicker2.spin(fwd, -100, pct);
+        }
       }
-      else{
-        kicker.spin(fwd,-100,pct);
-        kicker2.spin(fwd, -100,pct);
-      }
-    }
     }
     else if (con.ButtonL1.pressing())
     {
-      if (f5loop){
+      if (f5loop)
+      {
         f5loop = false;
-        punchythingToggle=!punchythingToggle;
+        punchythingToggle = !punchythingToggle;
       }
     }
     else
     {
       f5loop = true;
     }
-    if (!con.ButtonL2.pressing()){
-      f6loop=true;
+    if (!con.ButtonL2.pressing())
+    {
+      f6loop = true;
     }
     if (punchythingToggle)
     {
-      kicker.spin(fwd,100,pct);
-      kicker2.spin(fwd,100,pct);
+      kicker.spin(fwd, 100, pct);
+      kicker2.spin(fwd, 100, pct);
     }
-    else if (!con.ButtonL2.pressing()) {
+    else if (!con.ButtonL2.pressing())
+    {
       kicker.stop();
       kicker2.stop();
     }
-  
 
+    // leftwing control
+    if (con.ButtonRight.pressing())
+    {
+      if (f2loop)
+      {
+        lwingtoggle = !lwingtoggle;
+      }
+      if (lwingtoggle && f2loop)
+      {
+        leftwing.open();
+        f2loop = false;
+      }
+      if (!lwingtoggle && f2loop)
+      {
+        leftwing.close();
+        f2loop = false;
+      }
+    }
+    else
+    {
+      f2loop = true;
+    }
 
-   //leftwing control
-   if (con.ButtonRight.pressing())
-   {
-     if (f2loop)
-     {
-       lwingtoggle = !lwingtoggle;
-     }
-     if (lwingtoggle && f2loop)
-     {
-       leftwing.open();
-       f2loop = false;
-     }
-     if (!lwingtoggle && f2loop)
-     {
-       leftwing.close();
-       f2loop = false;
-     }
-   }
-   else
-   {
-     f2loop = true;
-   }
+    // rightwing control
+    if (con.ButtonY.pressing())
+    {
+      if (f1loop)
+      {
+        rwingtoggle = !rwingtoggle;
+      }
+      if (rwingtoggle && f1loop)
+      {
+        rightwing.open();
+        f1loop = false;
+      }
+      if (!rwingtoggle && f1loop)
+      {
+        rightwing.close();
+        f1loop = false;
+      }
+    }
+    else
+    {
+      f1loop = true;
+    }
 
-
-   //rightwing control
-   if (con.ButtonY.pressing())
-   {
-     if (f1loop)
-     {
-       rwingtoggle = !rwingtoggle;
-     }
-     if (rwingtoggle && f1loop)
-     {
-       rightwing.open();
-       f1loop = false;
-     }
-     if (!rwingtoggle && f1loop)
-     {
-       rightwing.close();
-       f1loop = false;
-     }
-   }
-   else
-   {
-     f1loop = true;
-   }
-
-
-   //right back wing control
-   if (con.ButtonB.pressing())
-   {
-     if (f3loop)
-     {
-       rbwingtoggle = !rbwingtoggle;
-     }
-     if (rbwingtoggle && f3loop)
-     {
-       rDropDown.open();
-       f3loop = false;
-     }
-     if (!rbwingtoggle && f3loop)
-     {
-       rDropDown.close();
-       f3loop = false;
-     }
-   }
-   else
-   {
-     f3loop = true;
-   }
- }
+    // right back wing control
+    if (con.ButtonB.pressing())
+    {
+      if (f3loop)
+      {
+        rbwingtoggle = !rbwingtoggle;
+      }
+      if (rbwingtoggle && f3loop)
+      {
+        rDropDown.open();
+        f3loop = false;
+      }
+      if (!rbwingtoggle && f3loop)
+      {
+        rDropDown.close();
+        f3loop = false;
+      }
+    }
+    else
+    {
+      f3loop = true;
+    }
+  }
 }
-
 
 int main()
 {
