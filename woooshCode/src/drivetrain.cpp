e737f0354260b9cff1907f961bd22461ce0903f2
almondkhokhar <153022020+almondkhokhar@users.noreply.github.com>
2024-04-25 00:01:19 -0400

    FullPIDattempt

--- woooshCode/src/drivetrain.cpp
+++ woooshCode/src/drivetrain.cpp
@@ -27,7 +27,10 @@ void drivetrainObj::move(double targetDistance, double timeout)
 {
   double distKp = 470.0;
   double turnKp = 470.0;
-  double distKi, distKd;
+  double distKi = 0.0;
+  double turnKi = 0.0;
+  double distKd = 0.0;
+  double turnKd = 0.0;
   double wheelDiameter = 2.75;
   // establishes when we started the procedure
   // records a starting position of the bot
@@ -35,20 +38,41 @@ void drivetrainObj::move(double targetDistance, double timeout)
   double startAng = Inertial.rotation(deg);
   double startPos = Drive.getEncoderValue();
   int startTime = vex::timer::system();
-
+  double errorSum = 0;
+  double turnErrorSum = 0;
+  bool firstLoop = true;
+  double turnPrevError;
+  double prevError;
+  double error= 0;
+  double turnErr = 0;
+  double velocity;
+  double turnVelocity;
   // limits the time the procedure can run
   while (vex::timer::system() - startTime < timeout * 1000)
   {
     // limits the speed so as the robot gets closer to where you want it it slows down the speed and doesn't overshoot the distance
+    if (firstLoop){
+      turnPrevError=turnErr;
+      prevError=error;
+      firstLoop=false;
+    }
+    else{
+      prevError=error;
+      turnPrevError=turnErr;
+    }
     double currentDistance = (Drive.getEncoderValue() - startPos) * M_PI / 180 * wheelDiameter / 2;
     double error = targetDistance - currentDistance;
-
     // calculates the difference between our current angle and our initial angle
     double turnErr = startAng - Inertial.rotation(deg);
-
-    double speed = error * distKp;
-    double turnSpeed = turnErr * turnKp;
-
+    errorSum += error;
+    turnErrorSum += turnErr;
+    velocity = (prevError-error)/.02;
+    turnVelocity = (turnPrevError-turnErr)/.02;
+    double speed = error * distKp; //+ errorSum * distKi -  velocity * distKd;
+    double turnSpeed = turnErr * turnKp; //+ turnErrorSum * turnKi - turnVelocity * turnKd;
+    printf("%lu\n", (velocity));
+    printf("%lu\n", (currentDistance));
+    printf("%lu\n", (targetDistance));
     // set the drive to the correct speed
     rightdrive.spin(fwd, speed - turnSpeed, vex::voltageUnits::mV);
     leftdrive.spin(fwd, speed + turnSpeed, vex::voltageUnits::mV);
@@ -94,13 +118,31 @@ void drivetrainObj::turn(double targetAngle, double timeout)
 {
   // establishes when the turn started
   int startTime = vex::timer::system();
-  double speed3;
+  double speed;
+  double Kp = .28;
+  double Ki = .28;
+  double Kd = .28;
+  double error;
+  double errorSum=0;
+  double prevError;
+  double velocity;
+  bool firstLoop = true;
   // limits the time so that it doesn't waste time fixing marginal error
   while (vex::timer::system() - startTime < timeout * 1000)
   {
-    speed3 = (targetAngle - Inertial.rotation(deg)) * .28;
-    rightdrive.spin(fwd, -speed3, pct);
-    leftdrive.spin(fwd, speed3, pct);
+    if (firstLoop){
+      prevError=error;
+      firstLoop=false;
+    }
+    else {
+      prevError= error;
+    }
+    errorSum += error;
+    velocity = (error-prevError)/.01;
+    error =(targetAngle - Inertial.rotation(deg));
+    speed = error * Kp + errorSum * Ki - velocity * Kd;
+    rightdrive.spin(fwd, -speed, pct);
+    leftdrive.spin(fwd, speed, pct);
     wait(10, msec);
     // limits the speed so as the robot gets closer to where you want it it slows down the speed and doesn't overshoot the distance
   }
--- woooshCode/src/main.cpp
+++ woooshCode/src/main.cpp
@@ -11,8 +11,8 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 
-// make class called apple and make apple.myapple can place properties about all of the apples, like ripeness, color, myapple.ripeness()=5 days, myapple2.ripeness()= 7 days, motor is a class for vex motors with properties such as spin, temperature, so you ccan make a class drivetrain with inputs of drivegroups left and right and then make a drivestraight function inside the drive class, odom applications: take wheel diameter input so when you had a previous wheel diameter for your robot you can change that parameter when you rebuild and all of your code still works. 
-// error is loss function, how far away are we from where we want to be. P value is just some number multiplied by your error and does most of the work to get you where you want to be. if p is to small your motor input will be to low to do anything and you won't reach the point you want to. if p is too high then you will reach the point you want to, but then you will overshoot it because you are going to fast so it will oscillate a lot. so you can try to get the P value very exact but its difficult and take a long time to tune, so what you can do is have it a little higher and then subtract your velocity. speed = error times kp - derivative of error times kd. what that does is calculate velocity and if we are moving faster move slower so we don't over shoot our target. dampening works by allowing us to not have to be super exact with our kp and dampen it's affect as we get closer to the target. when we are close to where we want to be but not eactly there the p value is basically 0 and the d value is basically 0 so you will be very close, but never exactly where you want to be. The I value which stands for integral is just the theoretical area under the graph of position vs time, telling you how long you've been that far away from your target. Derivative is current error and prev error starts at current error but after the first loop it becomes your current error, then current error updates, then you calculate your speed, and then your previous error. the way you track your integral is you take your error sum starting at 0 and every loop you just add your error to it, errorsum += error. so speed = p * kP + i * kI - d * kD, then tune it until you have each value affecting the program how you want, not just one individual part of it. no analytical way to find kp ki and kd so you have to take educated guesses and test your program until you have a program that functions how you want it to 
+// make class called apple and make apple.myapple can place properties about all of the apples, like ripeness, color, myapple.ripeness()=5 days, myapple2.ripeness()= 7 days, motor is a class for vex motors with properties such as spin, temperature, so you can make a class drivetrain with inputs of drivegroups left and right and then make a drivestraight function inside the drive class, odom applications: take wheel diameter input so when you had a previous wheel diameter for your robot you can change that parameter when you rebuild and all of your code still works. 
+// error is loss function, how far away are we from where we want to be. P value is just some number multiplied by your error and does most of the work to get you where you want to be. if p is to small your motor input will be to low to do anything and you won't reach the point you want to. if p is too high then you will reach the point you want to, but then you will overshoot it because you are going to fast so it will oscillate a lot. so you can try to get the P value very exact but its difficult and take a long time to tune, so what you can do is have it a little higher and then subtract your velocity. speed = error times kp - derivative of error times kd. what that does is calculate velocity and if we are moving faster move slower so we don't over shoot our target. dampening works by allowing us to not have to be super exact with our kp and dampen it's affect as we get closer to the target. when we are close to where we want to be but not eactly there the p value is basically 0 and the d value is basically 0 so you will be very close, but never exactly where you want to be. The I value which stands for integral is just the theoretical area under the graph of position vs time, telling you how long you've been that far away from your target. Derivative is current error and prev error starts at current error but after the first loop it becomes your current error, then current error updates, then you calculate your speed, and then your previous error. the way you track your integral is you take your error sum starting at 0 and every loop you just add your error to it, errorsum += error. so speed = error * kP + errorsum * kI - deriv of error * kD, then tune it until you have each value affecting the program how you want, not just one individual part of it. no analytical way to find kp ki and kd so you have to take educated guesses and test your program until you have a program that functions how you want it to 
 #include "vex.h"
 double DeltaL, DeltaR, DeltaB, currentL, currentR, PreviousL, PreviousR, DeltaTheta, X, Y, Theta, DeltaXSide, DeltaYSide, SideChord, OdomHeading;
 // initializes variables
@@ -210,6 +210,98 @@ int slowerDrop()
   wait(.25, sec);
   rDropDown.open();
   return (0);
+}
+void airplaneSkills(){
+  Inertial.setRotation(82, deg);// do this in preauto
+  Drive.move(-1000, .6);
+  Drive.turn(87, .4);
+  Drive.move(30, .8);
+  Drive.turn(-30, .7);
+  rDropDown.open();
+  kicker.spin(fwd, 70, pct);
+  kicker2.spin(fwd, 70, pct);
+  intake.spin(fwd, -100, pct);
+  wait(27, sec);
+  kicker.stop();
+  kicker2.stop();
+  rDropDown.close();
+  Drive.turn(45, .7);
+  Drive.move(25, .7);
+  Drive.turn(0, .5);
+  Drive.move(92, 1.8);
+  Drive.turn(-45, .6);
+  leftwing.open();
+  Drive.swing(30, .7, .6, 3, false);
+  Drive.move(1000, .4);
+  Drive.move(-12, .4);
+  Drive.move(1000, .4);
+  Drive.move(-12, .7);
+  leftwing.close();
+  intake.spin(fwd, 100, pct);
+  Drive.turn(-160, .9);
+  Drive.move(45, .95);
+  Drive.turn(-30, .5);
+  rightwing.open();
+  leftwing.open();
+  intake.spin(fwd, -100, pct);
+  Drive.swing(30, .75, .7, 3, true);
+  Drive.move(1000, .4);
+  Drive.move(-10, .3);
+  Drive.move(1000, .4);
+  rightwing.close();
+  leftwing.close();
+  Drive.move(-37, .8);
+  intake.spin(fwd, 100, pct);
+  Drive.turn(-30, .6);
+  rightwing.open();
+  leftwing.open();
+  intake.spin(fwd, -100, pct);
+  Drive.swing(30, .75, .7, 3, true);
+  Drive.move(1000, .5);
+  Drive.move(-10, .3);
+  Drive.move(1000, .4);
+  rightwing.close();
+  leftwing.close();
+  intake.spin(fwd, 100, pct);
+  Drive.move(-37, .8);
+  Drive.turn(-90, .7);
+  Drive.move(25, .7);
+  Drive.turn(45, .8);
+  rightwing.open();
+  leftwing.open();
+  intake.spin(fwd, -100, pct);
+  Drive.swing(45, 1, .5, 3, false);
+  Drive.move(1000, .5);
+  Drive.move(-10, .4);
+  Drive.move(1000, .5);
+  rightwing.close();
+  leftwing.close();
+  Drive.move(-25, .6);
+  intake.spin(fwd, 100, pct);
+  Drive.turn(-40, .7);
+  Drive.move(40, .8);
+  Drive.turn(45, .6);
+  rightwing.open();
+  intake.spin(fwd, -100, pct);
+  Drive.swing(40, .8, .4, 3, true);
+  Drive.move(1000, .5);
+  rightwing.close();
+  Drive.move(-20, .6);
+  Drive.turn(225, .8);
+  Drive.move(27, .7);
+  Drive.turn(180, .6);
+  hang.open();
+  intake.spin(fwd, -100, pct);
+  Drive.move(38, .8);
+  // kicker.spin(fwd, -100, pct);
+  // kicker2.spin(fwd, -100, pct);
+  // wait(3, sec);
+  // kicker.stop();
+  // kicker2.stop();
+  rDropDown.open();
+  wait(.04, sec);
+  rDropDown.close();
+
 }
 void AWPDefense()
 {
@@ -448,9 +540,12 @@ void fiveMidNear()
   printf("%lu\n", (vex::timer::system() - startTime));
 }
 
-
+void testing(){
+  Drive.move(10, 15);
+}
 void (*autonsList[])() =
 {
+  testing,
   fourMidNearset1,
   fourMidNearset2, 
   doNothing,
